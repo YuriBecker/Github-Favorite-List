94abd30b851770f5899b8e241110bc62cc1d213e
Yuri Becker <becker.yr@gmail.com>
2019-08-17 23:24:45 -0300

    Added sass suport

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
+import "./style.scss";
+
 const App = () => {
   useEffect(() => console.log(" Use Effect"), []);
   return (
     <>
-      <div>Hello React,Webpack 4 & Babel 7!</div>
+      <div>
+        <h1>Hello React,Webpack 4 & Babel 7!</h1>
+      </div>
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -58,15 +58,38 @@ module.exports = (env, argv) => ({
         ]
       },
       {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+          {
+            loader: require.resolve("postcss-loader"),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: "postcss",
+              plugins: () => [
+                require("postcss-flexbugs-fixes"),
+                autoprefixer({
+                  overrideBrowserslist: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9" // React doesn't support IE8 anyway
+                  ],
+                  flexbox: "no-2009"
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
         test: /\.css$/,
         use: [
-          require.resolve("style-loader"),
-          {
-            loader: require.resolve("css-loader"),
-            options: {
-              importLoaders: 1
-            }
-          },
+          { loader: "style-loader" },
+          { loader: "css-loader" },
           {
             loader: require.resolve("postcss-loader"),
             options: {
