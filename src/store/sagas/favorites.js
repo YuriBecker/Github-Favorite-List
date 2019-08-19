aa1324e8e2d6f04a1535719ee214d7968c27e3e8
Yuri Becker <becker.yr@gmail.com>
2019-08-19 09:27:50 -0300

    Added duplicated verification

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,19 +1,23 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 import { Creators as FavoriteActions } from '../ducks/favorites';
 
 export function* addFavorite({ payload }) {
 	try {
 		const { data } = yield call(api.get, `/repos/${payload.repository}`);
-
-		const repositoryData = {
-			id: data.id,
-			name: data.full_name,
-			description: data.description,
-			url: data.html_url
-		};
-
-		yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+		const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
+		let repositoryData = {};
+		if (isDuplicated) {
+			yield put(FavoriteActions.addFavoriteFailure('Error, repository duplicated!'));
+		} else {
+			repositoryData = {
+				id: data.id,
+				name: data.full_name,
+				description: data.description,
+				url: data.html_url
+			};
+			yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+		}
 	} catch (error) {
 		yield put(FavoriteActions.addFavoriteFailure('Error, repository not found!'));
 	}
